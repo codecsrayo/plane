591fc934cae54f4fecb69f8103f9073a33ea2fdd
Brayan <codecsrayo@gmail.com>
2026-04-28 03:20:46 +0000

    remove nix from root

--- .idx/dev.nix
+++
@@ -1,6 +0,0 @@
-args@{ ... }:
-
-# Wrapper requerido por Project IDX en esta ruta exacta.
-# La configuración real vive en nix/idx.nix para mantener
-# todas las definiciones de Nix agrupadas.
-import ../nix/idx.nix args
